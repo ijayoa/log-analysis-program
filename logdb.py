f701e5db7bdccdf7f2510815e86cd82996f81e49
Ijay <ij.oguegbu@gmail.com>
2018-10-20 12:35:52 -0300

    add most popular authors leaderboard

--- logdb.py
+++ logdb.py
@@ -2,41 +2,31 @@ import psycopg2
 
 
 def most_accessed_article():
-    """Return the most accessed articles of all time sorted in descending other."""
-    db = psycopg2.connect("dbname=news")  # connect to database
-    cursor = db.cursor()
-    try:
-        # query - retrieve articles and the amount of time they have been accessed
-        cursor.execute("select title, count(*) as views from articles, log where log.path like '/article/' ||  articles.slug and status = '200 OK' group by title order by views desc limit 3;")
-        rows = cursor.fetchall()  # fetch results from database
-        # loop through to display retrieved records
-        for i in rows:
-            # query - retrieve article titles via known path in log
-            print('"{}" - {} views'.format(i[0], i[1]))  # output string format
-    finally:
-        db.close()  # close database connection
+  """Return the most accessed articles of all time sorted in descending other."""
+  db = psycopg2.connect("dbname=news")  # connect to database
+  cursor = db.cursor()
+  try:
+    # query - retrieve articles and the amount of time they have been accessed
+    cursor.execute("select title, count(*) as views from articles, log where log.path like '/article/' ||  articles.slug and status = '200 OK' group by title order by views desc limit 3;")
+    rows = cursor.fetchall()  # fetch results from database
+    # loop through to display retrieved records
+    for i in rows:
+      # query - retrieve article titles via known path in log
+      print('"{}" - {} views'.format(i[0], i[1]))  # output string format
+  finally:
+    db.close()  # close database connection
 
-# def most_popular_authors():
-#   """Return the most popular authors sort by highest number of article views."""
-#   db = psycopg2.connect("dbname=news")
-#   cursor = db.cursor()
-#   try:
-#     leaderboard = {}
-#     cursor.execute('select id, name from authors;')
-#     rows_authors = cursor.fetchall()
-#     print(rows_authors)
-#     for i in rows_authors:
-#       cursor.execute("select id, slug from articles where author = {}".format(i[0]))
-#       authors_articles = cursor.fetchall()
-#       for i in authors_articles:
-#         cursor.execute("select count(*) from log where path = '/article/{}' and status = '200 OK'".format(i[1]))
-#         views = cursor.fetchall()
-#         print(views[0][])
-#         author_id = i[0]
-#         leaderboard[author_id] = len(authors_articles)
-#     print(leaderboard)
-#   finally:
-#     db.close()
+def most_popular_authors():
+  """Return the most popular authors sort by highest number of article views."""
+  db = psycopg2.connect("dbname=news")
+  cursor = db.cursor()
+  try:
+    cursor.execute("select authors.name, count(*) as views from authors inner join articles on authors.id = articles.author inner join log on status = '200 OK' and '/article/' || articles.slug = log.path group by authors.name order by views desc;")
+    leaderboard = cursor.fetchall()
+    for i in leaderboard:
+      print('{} - {} views'.format(i[0], i[1]))  # output string format
+  finally:
+    db.close()
 
 # def days_with_errors():
 #   db = psycopg2.connect("dbname=news")
@@ -53,11 +43,10 @@ print(80*"_")
 most_accessed_article()
 
 
-# print("\n")
-# print('The most popular article authors of all time:')
-# print(80*"_")
-# print("\n")
-# most_popular_authors()
+print("\n")
+print('The most popular article authors of all time:')
+print(80*"_")
+most_popular_authors()
 
 # print("\n")
 # print('Days with more than 1% of requests leading to errors:')
