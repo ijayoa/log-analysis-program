f2adae900fc7cdf04b7246112c7711afd1e9e7bf
vagrant <vagrant@vagrant.vm>
2018-10-22 17:45:59 +0000

    add she-bang

--- logdb.py
+++ logdb.py
@@ -1,49 +1,53 @@
+#!/usr/bin/python
+#!/usr/bin/env python2.7
+
 import psycopg2
 
 
 def most_accessed_article():
-    """Return the most accessed articles of all time sorted in descending other."""
+    """Most accessed articles of all time sorted in descending other."""
     db = psycopg2.connect("dbname=news")  # connect to database
     cursor = db.cursor()
     try:
-        # query : retrieve articles and the amount of time they have been accessed
-        cursor.execute("select title, count(*) as views from articles, log where log.path like '/article/' ||  articles.slug and status = '200 OK' group by title order by views desc limit 3;")
+        # query : articles and the amount of time they have been accessed
+        cursor.execute("select title, count(*) as views from articles, log where log.path like '/article/' ||  articles.slug and status = '200 OK' group by title order by views desc limit 3;")  # NOQA
         rows = cursor.fetchall()  # fetch results from database
         # loop through to display retrieved records
         for i in rows:
             # query : retrieve article titles via known path in log
-            print('"{}" - {} views'.format(i[0], i[1]))  # output string format
+            print('"{}" - {} views'.format(i[0], i[1]))  # output text
     finally:
         db.close()  # close database connection
 
 
 def most_popular_authors():
-    """Return the most popular authors sort by descending order highest total article views."""
+    """Most popular authors based on total article views per author."""
     db = psycopg2.connect("dbname=news")  # open database connection
     cursor = db.cursor()
     try:
         # query : retrieve author, find articles by them and count
-        cursor.execute("select authors.name, count(*) as views from authors inner join articles on authors.id = articles.author inner join log on status = '200 OK' and '/article/' || articles.slug = log.path group by authors.name order by views desc;")
+        cursor.execute("select authors.name, count(*) as views from authors inner join articles on authors.id = articles.author inner join log on status = '200 OK' and '/article/' || articles.slug = log.path group by authors.name order by views desc;")  # NOQA
         leaderboard = cursor.fetchall()  # fetch results from database
         # loop through to display retrieved records
         for i in leaderboard:
-            print('{} - {} views'.format(i[0], i[1]))  # output string format
+            print('{} - {} views'.format(i[0], i[1]))  # output text
     finally:
         db.close()  # close database connection
 
 
 def days_with_errors():
-    db = psycopg2.connect("dbname=news") # connect to database
-    cursor = db.cursor() # open cursor to scan records
+    """Percentage error of total requests per day."""
+    db = psycopg2.connect("dbname=news")  # connect to database
+    cursor = db.cursor()  # open cursor to scan records
     try:
-    		# query : use log_report view to calculate percent and filter those greater than 1
-        cursor.execute("select day, round(error * 100.0 / requests, 1) as percent from log_report group by day,error,requests having round(error * 100.0 / requests, 1) > 1 order by percent desc;")
-        rows = cursor.fetchall() # fetch results from database
+        # query:use log_report view to calculate percent daily errors > than 1
+        cursor.execute("select day, round(error * 100.0 / requests, 1) as percent from log_report group by  day,error,requests having round(error * 100.0 / requests, 1) > 1 order by percent desc;")  # NOQA
+        rows = cursor.fetchall()  # fetch results from database
         # loop through to display retrieved records
         for i in rows:
-            print('{} - {}% errors'.format(i[0], i[1]))  # output string format
+            print('{} - {}% errors'.format(i[0], i[1]))  # output text
     finally:
-        db.close() # close database connection
+        db.close()  # close database connection
 
 
 # report header format
